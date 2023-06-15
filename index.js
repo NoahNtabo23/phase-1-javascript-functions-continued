f6ddde1f65617463dfc0face18cb304f5fea450f
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-15 08:41:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity){
+    
+    if(!activity){
+        return "This Saturday, I want to roller-skate!"
+    }else {
+        return "This Saturday, I want to bathe my dog!"
+    }
+    
+    
+  
+    
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(activity){
+    if(!activity){
+        return "This Monday, I will go to the office."
+    }else{
+        return "This Monday, I will work from home."
+    }
+
+}
+mondayWork("work from home")
+
